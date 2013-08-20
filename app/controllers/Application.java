a6eb9266ddd3f849e175535b07aa497f46f90308
karthikguru <karthikguru88@gmail.com>
2013-08-20 13:12:36 +0530

    Included all AJAX methods

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.node.ObjectNode;
+
 import play.Logger;
 import play.Routes;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -13,9 +21,45 @@ public class Application extends Controller {
     }
     
     public static Result hello(String name){
-    	Logger.info("Inside Java Hello with name as: "+name );
-    	return ok("Hi "+name+", Have a great day..o...!!!!!");
+    	Logger.info("Inside Java Hello with name as: "+name);
+    	return ok("Hi " +name+ " greetings from JSRoutes and GET.....!!!!!");
     }
+    
+    public static Result helloPost(){
+    	Logger.info("Inside Java helloPost() method ");
+    	Logger.info(request().body().asFormUrlEncoded().get("name")[0]);
+    	Map<String,String[]> parameters = request().body().asFormUrlEncoded();
+    	String message = parameters.get("name")[0];
+    	return ok("Hi "+message+" greetings from AJAX POST.....!!!!!");
+    }
+    
+     public static Result getJson(String name){
+    	Logger.info("Inside Java getJson() method withn value :" +name);
+    	
+        Map<String,String> mapList1 = new HashMap<String,String>();
+        Map<String,String> mapList2 = new HashMap<String,String>();
+        
+    	List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+    	
+        mapList1.put("name", "karthikguru");
+        mapList1.put("age", "10");
+        list.add(mapList1);
+    	
+        mapList2.put("name", "Vinay");
+        mapList2.put("age", "2");
+        list.add(mapList2);
+        
+        //return ok(Json.toJson(list));
+        
+        ObjectNode on= Json.newObject();
+        on.put("name", name);
+        return ok(on);
+    }
+     
+     public static Result helloAjaxPlain(String name){
+     	Logger.info("Inside Java Hello with name as: "+name);
+     	return ok("Hi " +name+ " greetings from AJAX Plain.....!!!!!");
+     }
     
     public static Result myJsRoutes(){
     	response().setContentType("text/javascript");
--- public/javascripts/sayhello.js
+++ public/javascripts/sayhello.js
@@ -1,14 +1,75 @@
-function sayHello(){
-	var name = "karthik";
-	alert("before request");
+function selectHelloMethod(){
+	var name=$('#username').val();
+	var userSelect = Number($('#selectMethod').val());
+	
+	switch(userSelect){
+	case 1:
+		sayJsRoutesHello(name);
+		break;
+	case 2:
+		sayAjaxGetHello(name);
+		break;
+	case 3:
+		sayAjaxPostHello(name);
+		break;
+	case 4:
+		sayJsonHello(name);
+		break;
+	case 5:
+		sayAjaxPlainHello(name);
+		break;
+	default:
+		sayAjaxGetHello(name);
+		break;
+	}
+}
+
+function sayJsRoutesHello(name){
+	//alert("inside sayJsRoutesHello()");
 	myJsRoutes.controllers.Application.hello(name).ajax({
 		success: function(data){
-			alert("sucess");			
+			$('#sayHello').text(data);			
 		}
-		
-		error : function(){
-			alert("error");
-		}
+	});	
+}
+
+
+function sayAjaxGetHello(name){
+	//alert("inside sayAjaxGetHello()");
+	$.get('/hello',
+		  {'name':name},
+			function(data){
+			  $('#sayHello').text(data);
+		  });
+}
+
+function sayAjaxPlainHello(name){
+	$.ajax({url:'/ajax',
+			type: 'GET',
+		    data:{name : name},
+		    success: function(data){
+			      $('#sayHello').text(data);
+		    },
+		    error: function(jqXHR, textStatus, errorThrown){
+			      $('#sayHello').text(textStatus + " from error");
+		    }
 	});
-	}
+}
+
+function sayAjaxPostHello(name){
+	//alert("inside sayHelloPost()");
+	$.post('/hello',
+			{'name':name},
+			function(data){
+				$('#sayHello').text(data);
+			});
+}
+
+function sayJsonHello(name){
+	//alert("inside Json");
+	$.getJSON('/json',
+			  {"name":name},
+			  function(data){
+				 $('#sayHello').text("Hi " +data.name+ " greetings from Json.....!!!!!");
+			});
 }
